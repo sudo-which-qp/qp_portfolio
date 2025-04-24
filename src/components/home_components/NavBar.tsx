df480e425d51c7e1e91cded7effb9ebf5453ecbd
Godsend Joseph <bobbyjoseph108@gmail.com>
2025-04-24 19:46:03 +0100

    working on main contents

--- src/components/home_components/NavBar.tsx
+++ src/components/home_components/NavBar.tsx
@@ -1,67 +1,86 @@
 import { Avatar, Box, Flex, Icon, Link, Text } from "@chakra-ui/react";
 import { LuArrowUpRight } from "react-icons/lu";
+import qpImage from "../../assets/qp.jpg";
 
 function NavBar() {
-    return (
-        <Box width="100%" display="flex" justifyContent="center" padding={4}>
-        {/* Border Body */}
-        <Box
-          padding={3}
-          borderWidth="1px"
-          borderRadius="30px"
-          borderColor="gray.300"
-          width="50%"
+  return (
+    <Box
+      width="100%"
+      position="sticky"
+      display="flex"
+      justifyContent="center"
+      padding={4}
+      top="0"
+      zIndex="10"
+      bg={"whiteAlpha.900"}
+    >
+      {/* Border Body */}
+      <Box
+        padding={3}
+        borderWidth="1px"
+        borderRadius="30px"
+        borderColor="gray.300"
+        width="50%"
+      >
+        {/* Row for Header */}
+        <Flex
+          direction="row"
+          justifyContent="space-between"
+          maxWidth="1200px"
+          align="center"
         >
-          {/* Row for Header */}
-          <Flex
-            direction="row"
-            justifyContent="space-between"
-            maxWidth="1200px"
-            align="center"
-          >
-            {/* Row for Avatar */}
-            <Flex direction="row" align="center" gap="2">
-              <Avatar.Root>
-                <Avatar.Fallback name="Godsend Joseph" />
-                <Avatar.Image src="../assets/qp.jpg" />
-              </Avatar.Root>
-              <Text fontWeight="semibold">godsend.</Text>
-            </Flex>
-
-            {/* Row for Nav */}
-            <Flex direction="row" align="center" gap="3" paddingRight="3">
-              <Link href="#" download={false}>
-                <Flex direction="row" align="center" gap="0.2">
-                  <Text>Resume</Text>
-                  <Icon>
-                    <LuArrowUpRight />
-                  </Icon>
-                </Flex>
-              </Link>
-
-              <Link href="/gallery">
-                <Flex direction="row" align="center" gap="0.2">
-                  <Text>Gallery</Text>
-                  <Icon>
-                    <LuArrowUpRight />
-                  </Icon>
-                </Flex>
-              </Link>
-
-              <Link href="/blog">
-                <Flex direction="row" align="center" gap="0.2">
-                  <Text>Blog</Text>
-                  <Icon>
-                    <LuArrowUpRight />
-                  </Icon>
-                </Flex>
-              </Link>
-            </Flex>
-            {/* End Row for Nav */}
+          {/* Row for Avatar */}
+          <Flex direction="row" align="center" gap="2">
+            <Avatar.Root>
+              <Avatar.Fallback name="Godsend Joseph" />
+              <Avatar.Image src={qpImage} />
+            </Avatar.Root>
+            <Text fontWeight="semibold">godsend.</Text>
           </Flex>
-        </Box>
+
+          {/* Row for Nav */}
+          <Flex direction="row" align="center" gap="3" paddingRight="3">
+            <Link href="#" download={false}>
+              <Flex direction="row" align="center" gap="0.2">
+                <Text>Resume</Text>
+                <Icon>
+                  <LuArrowUpRight />
+                </Icon>
+              </Flex>
+            </Link>
+
+            <Link href="/gallery">
+              <Flex direction="row" align="center" gap="0.2">
+                <Text>Gallery</Text>
+                <Icon>
+                  <LuArrowUpRight />
+                </Icon>
+              </Flex>
+            </Link>
+
+            <Link href="/gallery">
+              <Flex direction="row" align="center" gap="0.2">
+                <Text>Papers</Text>
+                <Icon>
+                  <LuArrowUpRight />
+                </Icon>
+              </Flex>
+            </Link>
+
+            <Link href="/blog">
+              <Flex direction="row" align="center" gap="0.2">
+                <Text>Blog</Text>
+                <Icon>
+                  <LuArrowUpRight />
+                </Icon>
+              </Flex>
+            </Link>
+          </Flex>
+          {/* End Row for Nav */}
+        </Flex>
       </Box>
-    )
+    </Box>
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,27 @@
 import NavBar from "@/components/home_components/NavBar";
-
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 function HomePage() {
   return (
     <>
       <NavBar />
+
+      <Flex
+        width="100%"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box width="50%" paddingTop={10} alignItems="center">
+          <Text hyphens="auto" textStyle="3xl" textAlign="justify">
+            As a Full Stack Developer, I have been creating cross-platform
+            mobile applications for Android and iOS using Flutter, a technology
+            that I am proficient in and passionate about. I have also been
+            involved in designing and implementing robust APIs and web backends
+            using PHP Laravel
+          </Text>
+        </Box>
+      </Flex>
     </>
   );
 }
