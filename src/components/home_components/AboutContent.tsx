7d7f6670a7138f083fbda16162aaa8d1b3fd63ab
Godsend Joseph <bobbyjoseph108@gmail.com>
2025-08-08 04:08:28 +0100

    mod: updated about me text

--- src/components/home_components/AboutContent.tsx
+++ src/components/home_components/AboutContent.tsx
@@ -11,11 +11,19 @@ function AboutContent() {
             >
                 <Box width="90%" paddingTop={{base: 1, md: 8}} alignItems="center">
                     <Text px={4} fontFamily="roboto" textStyle={{base: "xl", md: "2xl", lg: "4xl"}} textAlign="center">
-                        Hi, I'm Godsend Joseph, a passionate and curious software engineer with hands-on experience in
-                        building web and mobile applications. I'm always exploring new technologies, solving real-world
-                        problems, and sharing what I learn. Currently pursuing my Master’s in Advanced Computer Science
-                        at Cardiff Metropolitan University, UK. Outside of tech, I enjoy reading, gaming, photography,
-                        and discovering new ways to grow.
+                        Hi, I’m Godsend Joseph. Full Stack Developer & MSc Advanced Computer Science Student
+                        I design and build secure, scalable web and mobile applications using Flutter, Laravel, Golang,
+                        and React. Over the past 4+ years, I’ve delivered fintech platforms, event management systems,
+                        and crypto payment solutions combining technical precision with user-focused design.
+                    </Text>
+                    <Text mt={6} px={4} fontFamily="roboto" textStyle={{base: "xl", md: "2xl", lg: "4xl"}} textAlign="center">
+                        Currently pursuing my MSc at Cardiff Metropolitan University, I’m exploring systems security,
+                        cloud-based architectures, and cutting-edge development practices. My goal is to create software
+                        that solves real-world problems and stands the test of scale and security.
+                    </Text>
+                    <Text mt={6} px={4} fontFamily="roboto" textStyle={{base: "xl", md: "2xl", lg: "4xl"}} textAlign="center">
+                        When I’m not coding, you’ll find me reading, gaming, exploring photography, or learning
+                        something new.
                     </Text>
                 </Box>
             </Flex>
